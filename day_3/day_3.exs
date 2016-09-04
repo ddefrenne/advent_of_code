7501090df1e50708eb97b75710feb3501ed19d2e
Dimitri De Frenne <ddefrenne@gmail.com>
2016-09-04 17:29:30 +0200

    Wait to filter the list until the end

--- day_3/day_3.exs
+++ day_3/day_3.exs
@@ -3,33 +3,25 @@ defmodule Day3 do
     number_of_houses(String.graphemes(input), [[x: 0, y: 0]])
   end
 
-  defp number_of_houses([], grid), do: Enum.count(grid)
+  defp number_of_houses([], grid) do
+    Enum.sort(grid)
+    |> Enum.dedup_by(fn([x, y]) -> [x,y] end)
+    |> Enum.count
+  end
 
   defp number_of_houses([ "^" | tail ], [ [x: last_x, y: last_y] | _] = grid) do
-    filtered_grid = Enum.filter(grid, fn([x: x, y: y]) -> 
-      !(x == last_x && y == (last_y + 1))
-    end) 
-    number_of_houses(tail, [[x: last_x, y: last_y + 1] | filtered_grid])
+    number_of_houses(tail, [[x: last_x, y: last_y + 1] | grid])
   end
 
   defp number_of_houses([ ">" | tail ], [ [x: last_x, y: last_y] | _] = grid) do
-    filtered_grid = Enum.filter(grid, fn([x: x, y: y]) -> 
-      !(x == (last_x + 1) && y == last_y)
-    end) 
-    number_of_houses(tail, [[x: last_x + 1, y: last_y] | filtered_grid])
+    number_of_houses(tail, [[x: last_x + 1, y: last_y] | grid])
   end
 
   defp number_of_houses([ "v" | tail ], [ [x: last_x, y: last_y] | _] = grid) do
-    filtered_grid = Enum.filter(grid, fn([x: x, y: y]) -> 
-      !(x == last_x && y == (last_y - 1))
-    end) 
-    number_of_houses(tail, [[x: last_x, y: last_y - 1] | filtered_grid])
+    number_of_houses(tail, [[x: last_x, y: last_y - 1] | grid])
   end
 
   defp number_of_houses([ "<" | tail ], [ [x: last_x, y: last_y] | _] = grid) do
-    filtered_grid = Enum.filter(grid, fn([x: x, y: y]) -> 
-      !(x == (last_x - 1) && y == last_y)
-    end) 
-    number_of_houses(tail, [[x: last_x - 1, y: last_y] | filtered_grid])
+    number_of_houses(tail, [[x: last_x - 1, y: last_y] | grid])
   end
 end
